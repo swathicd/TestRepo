a7be2909170480fb6c3f9e8572e6d530ae1e7ee5
Swathi Rajasekhar <mohan.swathi8101@gmail.com>
2015-06-04 20:22:09 +0100

    initial commit

---
+++ src/main/java/swathi/swathi_artifact/App.java
@@ -0,0 +1,13 @@
+package swathi.swathi_artifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
